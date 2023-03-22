60cd6ca4d9711cebc4003e224c23a5580e9177d3
PhantomaNexus <jacko.tread@hotmail.com>
2023-03-22 17:53:08 +0000

    Main Menu Added - Pickups fully working
    Main Menu Added - Pickups fully working

---
+++ GP 2 Hell/Assets/Scripts/UI_Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("OutdoorsScene");
+    }
+
+
+    public void MainMenuScreen()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Called when the player clicks on the "Quit" button in the game
+    public void QuitGame() { 
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+
+}
